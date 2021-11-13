6a11d30b16b5dbec865d50053b2e4c8ff5648c80
TatjanaSokolova <tsvm@inbox.lv>
2021-11-13 18:12:45 +0200

    2 projects completed

---
+++ scripts.js
@@ -0,0 +1,33 @@
+const path = 'assets/dodge1.png';
+let current_div = 'div1';
+let current_photo = "assets/dodge1.png";
+let current_photo_number = 1;
+let photo_count = 3;
+
+window.onbeforeunload = function () {
+  window.scrollTo(0, 0);
+}
+
+function replace_div( hide, show, photo_name ) {
+    if (hide == show) {
+        return;
+    }
+    document.getElementById(show).style.display="block";
+    document.getElementById(hide).style.display="none";
+    current_photo = photo_name;
+}
+
+function changediv_var( div ) {
+    current_div = div;
+}
+
+function replace_photo(id, photo_count) {
+    current_photo_number += 1;
+    if (current_photo_number > photo_count) {
+      document.getElementById(id).src = current_photo.substring(0, current_photo.length - 5) + "1.png";
+      current_photo_number = 1;
+    }
+    else {
+      document.getElementById(id).src = current_photo.substring(0, current_photo.length - 5) + current_photo_number+".png";
+    }
+}
